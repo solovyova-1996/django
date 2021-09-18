393581d42d8e27907fd9f86178c54492ea2c022c
solovyova-1996 <shatdar@yandex.ru>
2021-09-18 19:32:57 +0600

    dz-3-1

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='products_images')
+    short_desc = models.CharField(max_length=80, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
