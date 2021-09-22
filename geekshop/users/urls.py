ec0de13c2a13b04c7bdee535cc9cb52a36cc2793
solovyova-1996 <shatdar@yandex.ru>
2021-09-22 23:32:37 +0600

    adedd forms

--- geekshop/users/urls.py
+++ geekshop/users/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import login, register
+from .views import login, register, logout
 
 app_name = 'users'
 
@@ -23,4 +23,5 @@ urlpatterns = [
 
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 ]
--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
-from .forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from .forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,password=password)
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'Geekshop - Авторизация',
-        'form':form
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
     context = {
-        'title': 'Geekshop - Регистрация'
+        'title': 'Geekshop - Регистрация',
+        'form': form
     }
     return render(request, 'users/register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
