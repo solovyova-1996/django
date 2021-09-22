5200356beeba86a8441f4804d7f554329403c640
solovyova-1996 <shatdar@yandex.ru>
2021-09-22 16:00:47 +0600

    work with users app

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    context = {
+        'title': 'Geekshop - Авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Geekshop - Регистрация'
+    }
+    return render(request, 'users/register.html', context)
