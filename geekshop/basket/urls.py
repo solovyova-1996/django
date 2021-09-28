236ffda25a07ebb1d00140b6f1ba13c3c904cb5d
solovyova-1996 <shatdar@yandex.ru>
2021-09-28 18:21:09 +0600

    dz5

--- geekshop/basket/urls.py
+++ geekshop/basket/urls.py
@@ -22,5 +22,4 @@ app_name = 'basket'
 urlpatterns = [
     path('add/<int:product_id>', basket_add, name='basket'),
     path('remove/<int:product_id>', basket_remove, name='basket_remove'),
-
 ]
--- geekshop/basket/views.py
+++ geekshop/basket/views.py
@@ -4,6 +4,7 @@ from mainapp.models import Product
 from .models import Basket
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -16,7 +17,9 @@ def basket_add(request, product_id):
         basket.quantity += 1
         basket.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
 @login_required
-def basket_remove(request,product_id):
+def basket_remove(request, product_id):
     Basket.objects.get(id=product_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
