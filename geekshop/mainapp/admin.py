8acc00e1855b87878ea11ad36454a14eb4dec858
solovyova-1996 <shatdar@yandex.ru>
2021-10-03 01:01:01 +0600

    work from django admin

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from mainapp.models import ProductCategory, Product
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', ('price', 'quantity',), 'description', 'category',)
+    readonly_fields = ('description',)
+    ordering = ('name', 'price')
+    search_fields = ('name',)
