dbf1edda347610ce2506550d7009102008ffbc08
solovyova-1996 <shatdar@yandex.ru>
2021-10-06 20:33:16 +0600

    filter categories

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.shortcuts import render
 
 from .models import ProductCategory, Product
-
 
 
 def index(request):
@@ -10,14 +8,12 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
+    products = Product.objects.filter(category_id=category_id) if category_id != None else Product.objects.all()
     context = {
         'title': 'catalog',
         'categories': ProductCategory.objects.all(),
-        'products': Product.objects.all()
+        'products': products
     }
 
     return render(request, 'mainapp/products.html', context)
-
-
-
