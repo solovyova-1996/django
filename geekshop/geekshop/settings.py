ef131a386a4ced062d5835a726bad9cd5f5c4b76
solovyova-1996 <shatdar@yandex.ru>
2021-10-07 00:11:29 +0600

    create clases in users

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -130,3 +130,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
--- geekshop/users/urls.py
+++ geekshop/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import login, register, logout, profile
+from .views import LoginLoginView, LogoutView, RegisterListview, ProfileFormView
 
 app_name = 'users'
 
 urlpatterns = [
 
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, name='logout'),
-    path('profile/', profile, name='profile'),
+    path('login/', LoginLoginView.as_view(), name='login'),
+    path('register/', RegisterListview.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileFormView.as_view(), name='profile'),
 ]
