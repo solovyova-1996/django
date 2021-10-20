86e910f4da97e6c098a3907d771889286632db9b
solovyova-1996 <shatdar@yandex.ru>
2021-10-20 20:59:46 +0600

    add templates

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 
 class OrderList(ListView):
     pass
 
+
 class OrdersItemsCreate(CreateView):
+    pass
+
+
+class OrderUpdate(UpdateView):
+    pass
+
+
+class OrderDelete(DeleteView):
+    pass
+
+
+class OrderDetail(DetailView):
+    pass
+
+def order_forming_complete(request,pk):
     pass
