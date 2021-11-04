755480a36edc599e776e0827ac73c4a09c90248b
solovyova-1996 <shatdar@yandex.ru>
2021-11-04 13:43:49 +0600

    cache in maimnapp views

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 from geekshop.mixin import BaseClassContextMixin
-from .models import Product
+from .models import Product, ProductCategory
+from django.conf import settings
+from django.core.cache import cache
 
 
 class IndexView(ListView, BaseClassContextMixin):
@@ -22,7 +24,7 @@ class ProductListview(ListView, BaseClassContextMixin):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductListview, self).get_context_data(**kwargs)
-        context['products'] = Product.objects.all()
+        context['products'] = self.get_link_product()
         return context
 
     def get_queryset(self):
@@ -30,6 +32,29 @@ class ProductListview(ListView, BaseClassContextMixin):
             if 'category_id' in self.kwargs.keys():
                 return Product.objects.filter(category=self.kwargs['category_id'])
             elif 'discharge' in self.kwargs.keys():
-                return Product.objects.all()
+                return self.get_link_product()
         else:
-            return Product.objects.all()
+            return self.get_link_product()
+    @staticmethod
+    def get_link_product():
+        if settings.LOW_CACHE:
+            key = 'links_product'
+            link_products = cache.get(key)
+            if link_products is None:
+                link_products = Product.objects.all()
+                cache.set(key, link_products)
+            return link_products
+        else:
+            return Product.objects.all().select_related('category')
+
+
+# def get_links_category():
+#     if settings.LOW_CACHE:
+#         key = 'links_category'
+#         link_category = cache.get(key)
+#         if link_category is None:
+#             link_category = ProductCategory.objects.all()
+#             cache.set(key,link_category)
+#         return link_category
+#     else:
+#         return ProductCategory.objects.all()
