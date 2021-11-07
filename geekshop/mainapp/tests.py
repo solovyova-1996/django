247cce5c9d0f7812b771612af74513a20c1f02e3
solovyova-1996 <shatdar@yandex.ru>
2021-11-08 00:48:43 +0600

    add pytest

--- geekshop/mainapp/tests.py
+++ geekshop/mainapp/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test.client import Client
+
+from mainapp.models import ProductCategory, Product
+
+
+class TestMainSmokeTest(TestCase):
+    status_code_success = 200
+
+    # создание данных для теста
+    def setUp(self) -> None:
+        category = ProductCategory.objects.create(name='Test')
+        Product.objects.create(category=category, name='product_test', price=200)
+        self.client = Client()
+
+    # тест на проверку главной страницы
+    # Для того чтобы запустить тест python manage.py test и в названии функции вначале test
+    def test_products_pages(self):
+        response = self.client.get('/')
+        # Проверка на равенство
+        self.assertEqual(response.status_code, self.status_code_success)
+
+    def test_products_product(self):
+        for product_item in Product.objects.all():
+            response = self.client.get(f'/products/detail/{product_item.pk}/')
+            self.assertEqual(response.status_code, self.status_code_success)
+
+    # проверка есть ли перенаправление при входе в профил неавторизованного пользователя
+    def test_products_basket(self):
+        response = self.client.get('/users/profile/')
+        self.assertEqual(response.status_code, 302)
+
+    def tearDown(self) -> None:
+        pass
--- geekshop/users/tests.py
+++ geekshop/users/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+from geekshop import settings
+from users.models import User
+
+
+class TestMainSmokeTest(TestCase):
+    status_code_success = 200
+    status_code_render = 302
+    username = 'django'
+    email = 'dja@mail.ru'
+    password = 'geek1105'
+
+    new_user_data = {
+        'username': 'django1',
+        'first_name': 'Дарья',
+        'last_name': 'Шатилова',
+        'password1': 'geek1105',
+        'password2': 'geek1105',
+        'email': 'shat@mail.ru',
+    }
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_superuser(self.username, email=self.email, password=self.password)
+        self.client = Client()
+
+    def test_login(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get('/users/login/')
+        self.assertEqual(response.status_code, self.status_code_render)
+
+    def test_register(self):
+        response = self.client.get('/users/registers/', data=self.new_user_data)
+        self.assertEqual(response.status_code, self.status_code_render)
+        new_user = User.objects.get(username=self.new_user_data['username'])
+        print(new_user)
+        activation_url = f'{settings.DOMAIN_NAME}/users/verify/{self.new_user_data["email"]}/{new_user.activation_key}/'
+        response = self.client.get(activation_url)
+        self.assertEqual(response.status_code, self.status_code_success)
+        new_user.refresh_from_db()
+        self.assertTrue(new_user.is_active)
+
+    def tearDown(self) -> None:
+        pass
